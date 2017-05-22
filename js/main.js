b1deb28e9e3aa8f48dd9afdf3cccb07a305af6eb
PauLeeR <lindaliel@gmail.com>
2017-05-21 21:47:39 -0400

    problemas con los contenedores, muestra msj cannot read property appendChild de undefines pero consola no dice què es lo undefined

--- js/main.js
+++ js/main.js
@@ -1,18 +1,85 @@
 function validateForm(){
-	nombre = document.getElementById("name").value;
-	apellido = document.getElementById("lastname").value;
-	mail = document.getElementById("input-email").value;
-	password = document.getElementById("input-password").value;
-	bici = document.getElementsByClassName("form-control")[4].value;
+	var nombre = document.getElementById("name").value;
+	var apellido = document.getElementById("lastname").value;
+	var mail = document.getElementById("input-email").value;
+	var password = document.getElementById("input-password").value;
+	var bici = document.getElementById("bici").value;
 	//bike = document.getElementsByTagName("select");
-	twitter = document.getElementById("input-social").value;
+	//var twitter = document.getElementById("input-social").value;
 
-	function mayuscula(string){ // variable que contendra la validacion para que la primera letra sea mayuscula
+	/*function mayuscula(string){ // variable que contendra la validacion para que la primera letra sea mayuscula
 		return string.charAt(0).toUpperCase();
+	}*/
+	if (nombre == "" || nombre.length == 0 || /^[a-zA-Z]+$/.test(nombre) == false){
+		var spanName = document.createElement("span");
+		var textName = document.createTextNode("Este campo no debe estar vacío");
+		var contenedorNombre = document.getElementsByClassName("name-container input-box")[0];
+		spanName.appendChild(textName);
+		contenedorNombre.appendChild(spanName);
+		return false;
+	}else if(spanName != null){
+		contenedorNombre.removeChild(spanName);
+
+	}else if(nombre == "" || nombre.length == 0 || nombre.charAt(0) != nombre.charAt(0).toUpperCase()){
+		var spanNameMay = document.createElement("span");
+		var textNameMay = document.createTextNode("Tu nombre debe comenzar con mayúscula");
+		var contenedorNombre = document.getElementsByClassName("name-container input-box")[0];
+		spanNameMay.appendChild(textNameMay);
+		contenedorNombre.appendChild(spanNameMay);
+		return spanNameMay;
+	}
+	if (apellido == null || apellido.length == 0 ||  /^\s+$/.test(apellido)){
+		var spanLastn = document.createElement("span");
+		var textoSpanLastname = document.createTextNode("Este campo no debe estar vacío");
+		var contenedorApellido = document.getElementsByClassName("lastname-container input-box")[1];
+		spanLastn.appendChild(textoSpanLastname);
+		contenedorApellido.appendChild(spanLastn);
+		return false;
+
+	} else if(apellido == "" || apellido.charAt(0) != apellido.charAt(0).toUpperCase()){
+		var spanLastnMay = document.createElement("span");
+		var textoSpanLastnameMay = document.createTextNode("Tu apellido debe comenzar con mayúscula");
+		var contenedorApellido = document.getElementsByClassName("lastname-container input-box")[1];
+		spanLastnMay.appendChild(textoSpanLastnameMay);
+		contenedorApellido.appendChild(textoSpanLastnameMay);
+		return true;
+	}
+	if(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(mail) === false){
+		var spanEmail = document.createElement("span");
+		var textoSpanEmail = document.createTextNode("Este campo no debe estar vacío");
+		var contenedorEmail = document.getElementsByClassName("email-container input-box")[2];
+		spanEmail.appendChild(textoSpanEmail);
+		contenedorNombre.appendChild(spanEmail);
+		return false;	
+	} 
+	if (password == null || password.length == 0 || /^\s+$/.test(password)){ 
+		var spanPass = document.createElement("span");
+		var textoSpanPass = document.createTextNode("Este campo no debe estar vacio");
+		spanPass.appendChild(textoSpanPass);
+		contenedorPassword.appendChild(spanPass);
+		return false;
+	}
+	else if (password === "123456" || password === "098765" || password === "password" || password.length <= 6){
+		var spanPassSeguro = document.createElement("span");
+		var textoSpanPassSeguro = document.createTextNode("Para mayor seguridad, tu contraseña debe contener mínimo 6 caracteres");
+		var contenedorPassword = document.getElementsByClassName("form-group input-box")[3];
+		spanPassSeguro.appendChild(textoSpanPassSeguro);
+		contenedorPassword.appendChild(spanPassSeguro);
+		return false;
+	}
+	if(bici == null || bici == 0){
+		var spanBici = document.createElement("span");
+		var textoSpanBici = document.createTextNode("Selecciona una opción");
+		var contenedorBici = document.getElementsByClassName("form-group input-box")[4];
+		spanBici.appendChild(textoSpanBici);
+		contenedorBici.appendChild(spanBici);
+		return true;
 	}
 
+}
+
 //vamos  a crear distintos spans (cada uno en una funcion) para alertar al usuario si está cometiendo algún error al ingresar sus datos
-function spanName(){
+/*function spanName(){
 	var span = document.createElement("span");
 	var textoSpan = document.createTextNode("Tu nombre debe comenzar con mayúscula");
 	var contenedorNombre = document.getElementsByClassName("name-container input-box")[0];
@@ -68,6 +135,6 @@ function spanEmail(){
 			return;	
 		}
 	}
-}
-	
+}*/
+
 /* Escribe tú código aquí */
